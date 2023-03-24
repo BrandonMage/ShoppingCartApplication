1b7a01e4c92b1a0e1b5139f20f0d8be31ab35170
Brandon <128803583+BrandonMage@users.noreply.github.com>
2023-03-24 19:34:25 +0200

    Products list
    Hard-coded the list of products in the main method for the Product class. The user will be prompted to select a product from the list.

--- shop/Main.java
+++ shop/Main.java
@@ -7,6 +7,7 @@ class Product {
     String name;
     double price;
 
+    //    initializing the name and price properties
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
@@ -14,4 +15,23 @@ class Product {
 }
 
 public class Main {
+    public static void main(String[] args) {
+
+        //Array of hard-coded products
+        Product[] products = {
+                new Product("Mealie meal", 10.00),
+                new Product("Cooking oil", 20.00),
+                new Product("Ice cream", 30.00),
+                new Product("Chocolate", 40.00),
+                new Product("Milk", 50.00),
+                new Product("Juice", 60.00)
+        };
+
+        //Prompt the user to select a product and show their prices
+        System.out.println("Please select a product to add to your cart:");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i+1) + ". " + products[i].name + " - K" + products[i].price);
+        }
+
+    }
 }
